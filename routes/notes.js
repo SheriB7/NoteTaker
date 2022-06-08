3a67dac9296735f653eb609b4a9397c1b5e8a2d3
Sheri Brown <sherib70@gmail.com>
2022-06-07 20:12:27 -0400

    POST final update

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,53 @@
 const fb = require('express').Router();
 const fs = require('fs');
+const {nanoid} = require('nanoid');
 const file = './db/db.json'
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+  const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
 
 
 // GET Route for retrieving all the feedback
-fb.get('/', (req, res) => { 
+fb.get('/', (req, res) => {
 
-fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-         res.json(parsedData)
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            res.json(parsedData)
 
-    }
-  });
+        }
+    });
 
 });
 
 // POST Route for submitting feedback
 fb.post('/', (req, res) => {
-console.log("=======================================")
-console.log(req.body)
-console.log("=======================================")
-res.send("post api notes route")
+    const finalData = req.body
+    finalData.id = nanoid()
+    readAndAppend(req.body, file)
+
+
+
+    // console.log("=======================================")
+    // console.log(req.body)
+    // console.log("=======================================")
+    res.send("post api notes route")
 });
 
 module.exports = fb;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 
@@ -24,6 +22,8 @@ res.sendFile(path.join(__dirname, '/public/notes.html'))
 app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+
 
 
 app.listen(PORT, () =>
